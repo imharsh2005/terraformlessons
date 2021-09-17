7503f1faeacfcf641dc33128ff16af67c4b6beda
Sharma <Harshvardhan.Sharma@wolterskluwer.com>
2021-09-17 12:32:06 -0400

    remove the access key from github

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA3H57E6LIFJMYI4UR"
-  secret_key = "L0pMOigLJOmAMhToLaHUJaUfyRz0eUxxJy7Pio2C"
 }
 
 resource "aws_instance" "example"{
   ami = "ami-40d28157"
   instance_type = "t2.micro"
+  key
   tags = {
     Name = "Terraform-example"
   }
